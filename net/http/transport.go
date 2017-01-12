d3b03e42b8237506b5bd67953f857522c8f17f58
kshvakov <shvakov@gmail.com>
2017-01-12 12:17:15 +0200

    save request body for failback requests

--- net/http/transport.go
+++ net/http/transport.go
@@ -1,6 +1,10 @@
 package http
 
-import "net/http"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
 
 type Transport struct {
 	http.Transport
@@ -13,10 +17,11 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 		response *http.Response
 		service  = req.URL.Host
 	)
-
+	body, _ := ioutil.ReadAll(req.Body)
 	for i := 0; i <= _balancer.countOfBackends(service)*2; i++ {
 		if backend, err = _balancer.next(service); err == nil {
 			req.URL.Host = backend.address
+			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 			if response, err = t.Transport.RoundTrip(req); err == nil {
 				return response, nil
 			}
